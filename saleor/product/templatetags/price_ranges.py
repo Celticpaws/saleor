a208c34ac50afdb0e6cf9855c190588bc9bb2f7e
Anna Siwik <anna.siwik@mirumee.com>
2018-04-16 14:46:16 +0200

    Ability to configure if displayed taxes are gross or net

--- saleor/product/templatetags/price_ranges.py
+++ saleor/product/templatetags/price_ranges.py
@@ -3,6 +3,14 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag('product/_price_range.html')
-def price_range(price_range):
-    return {'price_range': price_range}
+@register.inclusion_tag('product/_price_range.html', takes_context=True)
+def price_range(context, price_range):
+    display_gross = context['site'].settings.display_gross_prices
+    return {'price_range': price_range, 'display_gross': display_gross}
+
+
+@register.simple_tag(takes_context=True)
+def price_to_display(context, price):
+    """Return price to display (gross or net) depending on settings."""
+    display_gross = context['site'].settings.display_gross_prices
+    return price.gross if display_gross else price.net
